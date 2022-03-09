81909e1357fc4e07d9e154701a12dc27bcf8fcc4
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2022-03-09 14:44:22 +0100

    useState - counter with simple object

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -4,18 +4,25 @@ import '../components/counter.css'
 
 export const CounterApp = () => {
 
-    const [counter, setCounter] = useState(10);
+    const [{counter1, counter2}, setCounter] = useState({
+        counter1: 10,
+        counter2: 20
+    });
 
   return (
     <>
-        <h1>Counter {counter}</h1>
+        <h1>Counter1 {counter1}</h1>
+        <h1>Counter2 {counter2}</h1>
         <hr />
 
         <button 
-        className="btn btn-primary"
-        onClick={() => {
-            setCounter(counter +1)
-        }}
+            className="btn btn-primary"
+            onClick={() => {
+                setCounter({
+                    counter1: counter1 +1,
+                    counter2: counter2
+                    })
+            }}
         >
             +1
         </button>
