80f8904619c4473ddcff0f66f87df790ed6a5fe7
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2022-03-09 18:10:28 +0100

    useEffect - Behavioral prevention with example / mousemove event

--- src/components/02-useEffect/Message.jsx
+++ src/components/02-useEffect/Message.jsx
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+  const [coords, setCoords] = useState({x: 0, y: 0});
+  const {x, y} = coords;
+
   useEffect(() => {
-    //Effect: Fase de >efecto
-    console.log('Componente montado')
+
+    const mouseMove = (e) => {
+        const coords = {x: e.x, y: e.y};
+        setCoords(coords);
+    }
+    
+    window.addEventListener('mousemove', mouseMove)
   
     return () => {
-      //Cleanup: Fase de >limpieza  
       console.log('Componente desmontado')
     }
-  }, []) //Input: Fase de >dependencias 
+  }, [])  
   
-
   return (
     <div>
         <p>Si name === '123', se mostrará este mensaje</p>
+        <p>Sólo si el párrafo anterior se cumple, veremos las coordenadas del mouse; si deja de cumplirse, dejaremos de verlas:</p>
+        <p>x: {x}, y: {y}</p>
     </div>
   )
 }
