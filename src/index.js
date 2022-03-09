549f66bc5ab0baaedca428ffa5c1ef1b9c6e0443
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2022-03-09 14:34:06 +0100

    useState - simple counter +1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HookApp } from './HookApp';
+import { CounterApp } from './components/CounterApp';
+//import { HookApp } from './HookApp';
 
 import './index.css';
 
 ReactDOM.render(
 
-    <HookApp />,
-  document.getElementById('root')
+    <CounterApp />,
+    document.getElementById('root')
 );
 
 
