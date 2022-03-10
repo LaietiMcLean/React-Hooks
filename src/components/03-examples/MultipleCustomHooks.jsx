facd15288122eea5344412baeeb90458b78a82f5
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2022-03-10 12:41:33 +0100

    useFetch (II) - showing data from api

--- src/components/03-examples/MultipleCustomHooks.jsx
+++ src/components/03-examples/MultipleCustomHooks.jsx
@@ -5,8 +5,9 @@ import '../02-useEffect/effects.css'
 
     export const MultipleCustomHooks = () => {
 
-        const {loading} = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-
+        const {loading, data} = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+        const {author, quote} = !!data && data[0]
+        console.log(author, quote);
         return (
             <div> 
                 <p>Breaking Bad Quotes</p>
@@ -20,13 +21,11 @@ import '../02-useEffect/effects.css'
                 :
                     (
                         <blockquote className='blockquote text-right'>
-                            <p className='mb-3'>Hola Mundo</p>
-                            <footer className='blockquote-footer'>Footer</footer>
+                            <p className='mb-3'>{quote}</p>
+                            <footer className='blockquote-footer'>{author}</footer>
                         </blockquote>
-
                     )
-                }
-                
+                } 
             </div>
         )
     }
